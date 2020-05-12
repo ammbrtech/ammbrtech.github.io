871896a8cc2a12179749e81e3c30764b8d8e9a38
niksmac <nikhil@lightrains.com>
2020-05-12 16:07:26 +0530

    First commit

---
+++ precache-manifest.2e53b55618f67e0a230d18e9cd3ece68.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca9217cf9cf74c22a75fc200454a9b84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cb06dd409024578193c",
+    "url": "/static/css/2.92d80790.chunk.css"
+  },
+  {
+    "revision": "21789b384abcb82dc923",
+    "url": "/static/css/main.06cf37ae.chunk.css"
+  },
+  {
+    "revision": "8cb06dd409024578193c",
+    "url": "/static/js/2.d72dc337.chunk.js"
+  },
+  {
+    "revision": "21789b384abcb82dc923",
+    "url": "/static/js/main.df765c1c.chunk.js"
+  },
+  {
+    "revision": "759f5ccf5faadbd0afd1",
+    "url": "/static/js/runtime-main.36011152.js"
+  },
+  {
+    "revision": "2d872348a0401c2ad75bbed678d7f4e9",
+    "url": "/static/media/2.2d872348.jpg"
+  },
+  {
+    "revision": "ea8eb97b7db43a00638310f14d9030cf",
+    "url": "/static/media/3.ea8eb97b.jpg"
+  },
+  {
+    "revision": "beba42ca5d779ff58dd5a91bde1a866d",
+    "url": "/static/media/4.beba42ca.jpg"
+  },
+  {
+    "revision": "23d984d01afc29656f04b5828e62cc8b",
+    "url": "/static/media/5.23d984d0.jpg"
+  },
+  {
+    "revision": "6816b3048fb20ea4104e6706dc7c9ead",
+    "url": "/static/media/about.6816b304.png"
+  },
+  {
+    "revision": "7818871ed9b01ac1ce0ed78f8883e969",
+    "url": "/static/media/contact.7818871e.png"
+  },
+  {
+    "revision": "361e901a1485be3c98e86618d2418751",
+    "url": "/static/media/s1.361e901a.png"
+  },
+  {
+    "revision": "6060b290bf44fa12b59e80c6f69513e0",
+    "url": "/static/media/s1.6060b290.png"
+  },
+  {
+    "revision": "9a03334de4e1a775e2b3e71807885bce",
+    "url": "/static/media/s2.9a03334d.png"
+  },
+  {
+    "revision": "671a0db8963521beeabe928ab3ebc2c3",
+    "url": "/static/media/ser1.671a0db8.jpg"
+  },
+  {
+    "revision": "c4b81e083250f54c3c1bc6bb9690666f",
+    "url": "/static/media/shape.c4b81e08.png"
+  },
+  {
+    "revision": "fff3d8271a92f850e0d7d46739599bb5",
+    "url": "/static/media/shape2.fff3d827.png"
+  },
+  {
+    "revision": "6ccedf52b475dc62aa998d287813230e",
+    "url": "/static/media/soft2.6ccedf52.png"
+  }
+]);
